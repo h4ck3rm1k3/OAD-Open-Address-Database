5150d89cb34654d81f69779adede305cc89ca610
James Michael DuPont <mdupont@introspector-mail.(none)>
2010-02-07 10:14:18 +0100

    update view

--- OAD/createcatalyst.sh
+++ OAD/createcatalyst.sh
@@ -1,2 +1,10 @@
+
+#create the basics
+catalyst.pl OAD
+
+cd OAD
+
 echo create the database scheme, user is $1 pass is $2
 script/oad_create.pl model CatalystModelName DBIC::Schema OAD::SchemaClass create=static dbi:mysql:OAD $1 $2
+
+script/oad_create.pl view TT TT
---
+++ OAD/lib/OAD/View/TT.pm
@@ -0,0 +1,31 @@
+package OAD::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+OAD::View::TT - TT View for OAD
+
+=head1 DESCRIPTION
+
+TT View for OAD. 
+
+=head1 SEE ALSO
+
+L<OAD>
+
+=head1 AUTHOR
+
+James Michael DuPont,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
